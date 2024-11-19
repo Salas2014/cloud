4da91da3443927a8e431915d57fa85f2fafc521d
Vladyslav Hasporian <saslasengland33@gmail.com>
2024-11-19 22:12:30 +0200

    add discovery euruka servece; add dependency on person 'discovery-client' and enable annotation; add to config service config for person connect ro eureka

--- person-service/src/main/java/org/salas/person/service/PersonServiceApplication.java
+++ person-service/src/main/java/org/salas/person/service/PersonServiceApplication.java
@@ -2,7 +2,9 @@ package org.salas.person.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class PersonServiceApplication {
 
